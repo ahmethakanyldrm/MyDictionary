c34a869f13d6a49df2d6ba69a40583bedafda73a
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2021-01-21 23:50:18 +0300

    MyDictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] plateNumbers;
+        V[] cities;
+
+        public MyDictionary()
+        {
+            plateNumbers = new K[0];
+            cities = new V[0];
+        }
+        public void Add(K plateNumber, V city)
+        {
+            K[] tempPlateNumber = plateNumbers;
+            V[] tempCities = cities;
+
+            plateNumbers = new K[plateNumbers.Length + 1];
+            cities = new V[cities.Length + 1];
+
+            for (int i = 0; i < tempPlateNumber.Length; i++)
+            {
+                plateNumbers[i] = tempPlateNumber[i];
+                cities[i] = tempCities[i];
+            }
+            plateNumbers[plateNumbers.Length - 1] = plateNumber;
+            cities[cities.Length - 1] = city;
+        }
+
+        public K[] PlateNumbers
+        {
+            get
+            {
+                return plateNumbers;
+            }
+        }
+
+        public V[] Cities
+        {
+            get
+            {
+                return cities;
+            }
+        }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+            cities.Add(23, "Elazığ");
+            cities.Add(19, "Çorum");
+            cities.Add(31, "Hatay");
+            cities.Add(42, "Konya");
+            cities.Add(47, "Mardin");
+            cities.Add(45, "Manisa");
+            cities.Add(41, "Kocaeli");
+
+            foreach (var city in cities.Cities)
+            {
+                Console.WriteLine("Şehir : " + city);
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------");
+            Console.WriteLine();
+            foreach (var plateNumber in cities.PlateNumbers)
+            {
+                Console.WriteLine("Plaka Kodu  : " + plateNumber);
+            }
+            Console.WriteLine();
+          
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
